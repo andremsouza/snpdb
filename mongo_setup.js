f9f435424eda0cc65d0ef65098d111f016137589
Rodrigo Weigert <rodrigo.neorooker@gmail.com>
2019-09-17 17:23:21 -0300

    Updated mongo_setup.js to use config.js.

--- mongo_setup.js
+++ mongo_setup.js
@@ -1,29 +1,5 @@
 
-const HOST = "localhost/snpdb";
-
-/* Names for collections and their indexed attributes. 
-Single-letter attribute names save space on dense collections. */
-
-const SNPS_COLL = "snps";
-const SNPS_ID_LIST_ATTR = "i";
-const SNPS_CHROMOSOME_ATTR = "c";
-const SNPS_POSITION_ATTR = "p";
-const SNPS_MAPS_ATTR = "m";
-
-const INDIVIDUALS_COLL = "individuals";
-const INDIVIDUALS_ID_LIST_ATTR = "tatoos";
-const INDIVIDUALS_SAMPLE_LIST_ATTR = "samples";
-
-const SNPBLOCKS_COLL = "snpblocks";
-const SNPBLOCKS_SAMPLE_ATTR = "s";
-const SNPBLOCKS_SNP_LIST_ATTR = "a";
-const SNPBLOCKS_SNP_ID_INSIDE_LIST = "i";
-
-const MAPS_COLL = "maps";
-const MAPS_SNP_LIST_ATTR = "snps";
-
-const COUNTERS_COLL = "counters";
-const COUNTERS_SEQ_VALUE_ATTR = "next";
+load("config.js");
 
 /* Creates an object {key1: value1, key2: value2...}
 to be passed to MongoDB functions. */
@@ -39,23 +15,23 @@ function keyValueObject(key1, value1, ...args)
 }
 
 
-let db = connect(HOST);
+let db = connect(config.HOST);
 
-db.createCollection(SNPS_COLL);
-db[SNPS_COLL].createIndex(keyValueObject(SNPS_ID_LIST_ATTR, 1));
-db[SNPS_COLL].createIndex(keyValueObject(SNPS_CHROMOSOME_ATTR, 1, SNPS_POSITION_ATTR, 1));
-db[SNPS_COLL].createIndex(keyValueObject(SNPS_MAPS_ATTR, 1));
+db.createCollection(config.SNPS_COLL);
+db[config.SNPS_COLL].createIndex(keyValueObject(config.SNPS_ID_LIST_ATTR, 1));
+db[config.SNPS_COLL].createIndex(keyValueObject(config.SNPS_CHROMOSOME_ATTR, 1, config.SNPS_POSITION_ATTR, 1));
+db[config.SNPS_COLL].createIndex(keyValueObject(config.SNPS_MAPS_ATTR, 1));
 
-db.createCollection(INDIVIDUALS_COLL);
-db[INDIVIDUALS_COLL].createIndex(keyValueObject(INDIVIDUALS_ID_LIST_ATTR, 1));
-db[INDIVIDUALS_COLL].createIndex(keyValueObject(INDIVIDUALS_SAMPLE_LIST_ATTR, 1), unique=true);
+db.createCollection(config.INDIVIDUALS_COLL);
+db[config.INDIVIDUALS_COLL].createIndex(keyValueObject(config.INDIVIDUALS_ID_LIST_ATTR, 1));
+db[config.INDIVIDUALS_COLL].createIndex(keyValueObject(config.INDIVIDUALS_SAMPLE_LIST_ATTR, 1), unique=true);
 
-db.createCollection(SNPBLOCKS_COLL);
-db[SNPBLOCKS_COLL].createIndex(keyValueObject(SNPBLOCKS_SAMPLE_ATTR, 1, SNPBLOCKS_SNP_LIST_ATTR + ".0." + SNPBLOCKS_SNP_ID_INSIDE_LIST, 1));
+db.createCollection(config.SNPBLOCKS_COLL);
+db[config.SNPBLOCKS_COLL].createIndex(keyValueObject(config.SNPBLOCKS_SAMPLE_ATTR, 1, config.SNPBLOCKS_SNP_LIST_ATTR + ".0." + config.SNPBLOCKS_SNP_ID_INSIDE_LIST, 1));
 
-db.createCollection(MAPS_COLL);
-db[MAPS_COLL].createIndex(keyValueObject(MAPS_SNP_LIST_ATTR, 1));
+db.createCollection(config.MAPS_COLL);
+db[config.MAPS_COLL].createIndex(keyValueObject(config.MAPS_SNP_LIST_ATTR, 1));
 
 /* Collection for generating sequential ids. */
-db.createCollection(COUNTERS_COLL);
-db[COUNTERS_COLL].insert(keyValueObject("_id", SNPS_COLL, COUNTERS_SEQ_VALUE_ATTR, NumberInt(0)));
+db.createCollection(config.COUNTERS_COLL);
+db[config.COUNTERS_COLL].insert(keyValueObject("_id", config.SNPS_COLL, config.COUNTERS_SEQ_VALUE_ATTR, NumberInt(0)));
