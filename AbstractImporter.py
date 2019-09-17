8f510ac6b7785824b6707d6e364fff75fc3c71da
Rodrigo Weigert <rodrigo.neorooker@gmail.com>
2019-09-17 17:56:43 -0300

    Fixed syntax error and improved AbstractImporter.

--- AbstractImporter.py
+++ AbstractImporter.py
@@ -3,6 +3,7 @@ import json
 
 class AbstractImporter(ABC):
 
+
     def read_config(self):
         s = "{\n"
         with open("config.js", "r") as f:
@@ -11,19 +12,22 @@ class AbstractImporter(ABC):
                 s += line + "\n"
         return json.loads(s)
 
+
     def __init__(self):
         try:
-            for (k, v) in self.read_config():
+            for (k, v) in self.read_config().items():
                 setattr(self, k, v)
         except FileNotFoundError:
-            raise Exception("config.js not found.")
-        except JSONDecodeError:
-            raise Exception("Syntax error on config.js.")
+            raise Exception("config.js not found.") from None
+        except json.decoder.JSONDecodeError:
+            raise Exception("Syntax error on config.js.") from None
+
 
     @abstractmethod
-    def import_map(self):
+    def import_map(self, filename):
         pass
 
+
     @abstractmethod
-    def import_sample(self):
+    def import_sample(self, filename):
         pass
