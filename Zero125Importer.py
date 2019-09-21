b5aebbded28259de92e5d4b462ccd41ea495d7f3
Rodrigo Weigert <rodrigo.neorooker@gmail.com>
2019-09-21 15:51:50 -0300

    Added Zero125Importer.

---
+++ Zero125Importer.py
@@ -0,0 +1,30 @@
+from AbstractImporter import AbstractImporter
+
+class Zero125Importer(AbstractImporter):
+    def read_map(self, filename):
+        snps = []
+        with open(filename, "r") as f:
+            next(f)
+            for line in f:
+                (id, chrom, pos) = line.split()
+                current = {
+                    self.SNPS_NAME_ATTR: id,
+                    self.SNPS_CHROMOSOME_ATTR: chrom,
+                    self.SNPS_POSITION_ATTR: pos
+                }
+                snps.append(current)
+        return snps
+
+    def read_samples(self, filename):
+        samples = []
+        with open(filename, "r") as f:
+            next(f)
+            for line in f:
+                (id, genotype) = line.split()
+                g = [{
+                    self.SNPBLOCKS_SNP_GENOTYPE_INSIDE_LIST: genotype[i]}
+                    for i in range(len(genotype))]
+                samples.append(
+                    {self.SAMPLE_DICT_ID: id,
+                    self.SAMPLE_DICT_GENOTYPE: g})
+        return samples
